3dec0742a2e345c9c34e540da3d3b07274649c2d
Ludo <77570058+LudoENMA@users.noreply.github.com>
2023-04-26 19:35:23 +0200

    Delete Script_SQL.sql

--- Script_SQL.sql
+++
@@ -1,9 +0,0 @@
-use customer_satisfaction;
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/inf_company.csv'
-IGNORE INTO TABLE info_company
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-select * from info_company;
-SELECT name_company, nombre_avis,  localisation_company FROM info_company WHERE localisation_company="Paris";
